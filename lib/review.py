766082fd1c472e6a1b5458c1a6f516b895589b98
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-12-19 21:53:15 +0300

    Returns Review instance from table row

--- lib/review.py
+++ lib/review.py
@@ -101,7 +101,12 @@ class Review:
     @classmethod
     def find_by_id(cls, id):
         """Return a Review instance having the attribute values from the table row."""
-        pass
+        sql = '''
+            SELECT * FROM reviews
+            WHERE id = ?
+        '''
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row)
 
     def update(self):
         """Update the table row corresponding to the current Review instance."""
